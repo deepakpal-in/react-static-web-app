79588164557ee413963c080d008d35904cdd6560
Deepak Pal <deepakpalymca@gmail.com>
2024-05-07 16:51:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'Running...!';
+  return <div>React Frontend CICD Status: {value}</div>;
 }
 
 export default App;
